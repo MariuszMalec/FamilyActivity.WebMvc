e4295e39d84bfd69be71be1fd430f09dbd284ad6
MariuszMalec <mario106c@gmail.com>
2024-05-25 22:56:35 +0200

    add PictureActivityController and dockerdtabase.yml

---
+++ Controllers/PictureActivityController.cs
@@ -0,0 +1,25 @@
+﻿using FamilyActivity.WebMvc.Contexts;
+using FamilyActivity.WebMvc.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyActivity.WebMvc.Controllers
+{
+    public class PictureActivityController : Controller
+    {
+        private readonly ApplicationContext _context;
+        private IActivityService _activityService;
+
+        public PictureActivityController(ApplicationContext context, IActivityService activityService)
+        {
+            _context = context;
+            _activityService = activityService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _activityService.GetAll();
+
+            return View(model);
+        }
+    }
+}
